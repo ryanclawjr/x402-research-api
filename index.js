0bb7e22f5db5f5caf99b6a703df116fffea4751b
Ryan T Hawks <ryanthawks@Ryans-Mac-mini.local>
2026-02-13 23:14:05 -0500

    Upgrade to x402 v2 (@x402/express)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { paymentMiddleware } = require("x402-express");
+const { paymentMiddleware } = require("@x402/express");
 const cors = require("cors");
 
 const app = express();
